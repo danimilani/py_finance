fe6f4e9672990a441a16477c737f67ec2dc072cf
Daniella Milani <58880353+danimilani@users.noreply.github.com>
2023-04-09 11:37:27 +0100

    Update 6_datetimes.py

--- 6_datetimes.py
+++ 6_datetimes.py
@@ -1,5 +1,7 @@
 # Datetimes and Dictionaries
 
+# DATETIME #
+
 # Datetime Module and Objects
 
 # The objects have attributes representing year, month, date, hour, second, microsecond and timezone
@@ -142,6 +144,148 @@ print(new_text)
     1992-09-16 00:00:00
     Wednesday, September 16, 1992
 """
+# Comparing datetimes
+
+from datetime import datetime
+asian_crisis = datetime(1997,7,2)
+world_mini_crash = datetime(1997,10,27)
+
+asian_crisis > world_mini_crash
+# Result : False
+
+asian_crisis < world_mini_crash
+# Result : True
+
+text = "10/27/1997"
+format_str = "%m/%d/%Y"
+sell_date = datetime.strptime(text, format_str)
+
+sell_date == world_mini_crash
+# Result: true
+
+# Time Delta
+
+delta = world_mini_crash - asian_crisis
+
+type(delta)
+# Result: datetime.timedelta
+
+delta.days
+# Result: 117
+
+# Creating Relative Datetimes
+# Create your own timedelta and use it as an offset to create a relative datetime
+
+from datetime import timedelta
+offset = timedelta(weeks = 1)
+offset
+# result: datetime.timedelta(7)
+
+dt - offset
+# result: datetime.datetime(2019,1,7,0,0)
+
+offset = timedelta(days = 16)
+dt - offset
+# result: datetime.datetime(2019,12,29,0,0)
+
+cur_week = last_week + timedelta(weeks = 1)
+# Do some work with date
+# set last week variable to cur week and repeat
+last_week = cur_week
+
+source_dt = event_dt - timedelta(weeks=4)
+# Use source datetime to look up market factors
+
+########################################################
+
+# EXERCISES #
+
+## Accessing datetime attributes
+
+# March 10, 2000 Tech Bubble Crash
+tech_bubble = datetime(2000, 3, 10)
+
+# Access the year
+yr  = tech_bubble.year
+
+# Access the month
+mth = tech_bubble.month
+
+# Access the day
+day = tech_bubble.day
+
+print(f"Year: {yr}, Month: {mth}, Day: {day}")
+
+<script.py> output:
+    Year: 2000, Month: 3, Day: 10
+
+## Comparing datetimes
+
+# Lehman Brothers before Morgan Stanley?
+lehman_first = lehman < morgan_stanley
+print(f"It is {lehman_first} that Lehman Brothers declared bankruptcy first.") 
+<script.py> output:
+    It is True that Lehman Brothers declared bankruptcy first.
+
+# Goldman Sachs after TARP?
+tarp_first = goldman_sachs > tarp
+print(f"It is {tarp_first} that TARP was approved first.")
+<script.py> output:
+    It is False that TARP was approved first.
+
+# Goldman Sachs and Morgan Stanley same day?
+same_time = goldman_sachs == morgan_stanley
+print(f"It is {same_time} that Morgan Stanley and Goldman Sachs acted simultaneously")
+<script.py> output:
+    It is True that Morgan Stanley and Goldman Sachs acted simultaneously
+
+## Making relative datetimes
+
+# Create a datetime for one week before TARP was passed
+from datetime import datetime, timedelta
+
+# TARP passed Oct 3 2008
+tarp = datetime(2008, 10, 3)
+
+# Seven days before TARP
+week_before = tarp - timedelta(days = 7)
+
+# Print week_before
+print(week_before)
+<script.py> output:
+    2008-09-26 00:00:00
+
+# Create a datetime for one week after TARP was passed
+from datetime import datetime, timedelta
+
+# TARP passed Oct 3 2008
+tarp = datetime(2008, 10, 3)
+
+# One week after TARP
+week_after = tarp + timedelta(weeks = 1)
+
+# Print week_after
+print(week_after)
+<script.py> output:
+    2008-10-10 00:00:00
+
+# Create a datetime for one year after TARP was passed
+from datetime import datetime, timedelta
+
+# TARP passed Oct 3 2008
+tarp = datetime(2008, 10, 3)
+
+# One year after TARP
+year_after = tarp + timedelta(weeks = 52)
+
+# Print year_after
+print(year_after)
+<script.py> output:
+    2009-10-02 00:00:00
+
+########################################################
+
+# DICTIONARIES #
 
 
 
