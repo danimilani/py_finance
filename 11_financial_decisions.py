08d58810435dc67bfee8f09efbe3a31fb5d6a9bd
Daniella Milani <58880353+danimilani@users.noreply.github.com>
2023-05-28 12:29:05 +0000

    Update 11_financial_decisions.py

--- 11_financial_decisions.py
+++ 11_financial_decisions.py
@@ -119,6 +119,95 @@ print("WACC: " + str(round(100*wacc, 2)) + "%")
 
 #<script.py> output: WACC: 12.9%
 
+# EXERCISE - Comparing project NPV with IRR
+
+import numpy as np
+
+# Set your weighted average cost of capital equal to 12.9%
+wacc = 0.129
+
+# Calculate the net present value for Project 1
+npv_project1 = np.npv(wacc, cf_project1)
+print("Project 1 NPV: " + str(round(npv_project1, 2)))
+
+# Calculate the net present value for Project 2
+npv_project2 = np.npv(wacc, cf_project2)
+print("Project 2 NPV: " + str(round(npv_project2, 2)))
+
+##Project 1 NPV: 856073.18
+##Project 2 NPV: 904741.35
+
+## Comparing two projects of different life spans
+
+# EAA - Equivalent Annual Annuity can be used to compare 2 projects of diff
+# lifespans in present values terms
+
+import numpy as np
+npv_proj1 = 362.58
+npv_proj2 = 453.64
+
+np.pmt(rate=0.05, nper=8, pv=-1*npv_proj1, fv=0)
+#result = 133.14
+
+np.pmt(rate=0.05, nper=8, pv=-1*npv_proj2, fv=0)
+#result = 70.18
+
+# EXERCISE - Two project with different lifespans
+
+import numpy as np
+
+# Create a numpy array of cash flows for Project 1
+cf_project_1 = np.array([-700,100,150,200,250,300,350,400])
+
+# Create a numpy array of cash flows for Project 2
+cf_project_2 = np.array([-400,50,100,150,200,250,300])
+
+# Scale the original objects by 1000x
+cf_project1 = cf_project_1 * 1000
+cf_project2 = cf_project_2 * 1000
+
+# Calculating IRR and NPV with different project lifespans
+
+import numpy as np
+
+# Calculate the IRR for Project 1
+irr_project1 = np.irr(cf_project1)
+print("Project 1 IRR: " + str(round(100*irr_project1, 2)) + "%")
+
+# Calculate the IRR for Project 2
+irr_project2 = np.irr(cf_project2)
+print("Project 2 IRR: " + str(round(100*irr_project2, 2)) + "%")
+
+# Set the wacc equal to 12.9%
+wacc = 0.129
+
+# Calculate the NPV for Project 1
+npv_project1 = np.npv(wacc, cf_project1)
+print("Project 1 NPV: " + str(round(npv_project1, 2)))
+
+# Calculate the NPV for Project 2
+npv_project2 = np.npv(wacc, cf_project2)
+print("Project 2 NPV: " + str(round(npv_project2, 2)))
+
+##Project 1 IRR: 22.94%
+##Project 2 IRR: 26.89%
+##Project 1 NPV: 302744.98
+##Project 2 NPV: 231228.39
+
+# Using the equivalent annual annuity approach
+
+import numpy as np
+
+# Calculate the EAA for Project 1
+eaa_project1 = np.pmt(rate=wacc, nper=8, pv=npv_project1, fv=0)
+print("Project 1 EAA: " + str(round(eaa_project1, 2)))
+
+# Calculate the EAA for Project 2
+eaa_project2 = np.pmt(rate=wacc, nper=7, pv=npv_project2, fv=0)
+print("Project 2 EAA: " + str(round(eaa_project2, 2)))
+
+#Project 1 EAA: 62872.2
+#Project 2 EAA: 52120.61
 
 
 
